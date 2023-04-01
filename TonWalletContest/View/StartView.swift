0b961e1e7913d1759e46dc1a8d8b685115de0e67
elyahk <eldorbek.nusratov.98@gmail.com>
2023-04-02 01:53:26 +0200

    Make last support version to `v14`

---
+++ TonWalletContest/View/StartView.swift
@@ -0,0 +1,24 @@
+//
+//  StartView.swift
+//  TonWalletContest
+//
+//  Created by eldorbek nusratov on 02/04/23.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        NavigationView {
+            NavigationLink("Create Wallet App") {
+
+            }
+        }
+    }
+}
+
+struct StartView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartView()
+    }
+}
