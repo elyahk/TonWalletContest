8f439d8d4ab733cbb42176464e1be31a748da8e4
elyahk <eldorbek.nusratov.98@gmail.com>
2023-04-02 01:28:44 +0200

    Initial Commit

---
+++ TonWalletContest/TonWalletContestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TonWalletContestApp.swift
+//  TonWalletContest
+//
+//  Created by eldorbek nusratov on 02/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TonWalletContestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TonWalletContestUITests/TonWalletContestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TonWalletContestUITestsLaunchTests.swift
+//  TonWalletContestUITests
+//
+//  Created by eldorbek nusratov on 02/04/23.
+//
+
+import XCTest
+
+final class TonWalletContestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
