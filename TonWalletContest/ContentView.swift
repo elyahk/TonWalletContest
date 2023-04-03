9ef3eb6c9bd346bed0dc777a7d2b7639f1497109
elyahk-apple-academy <enusratov22@fed.idserve.net>
2023-04-03 17:13:29 +0200

    Add TonWalletManager

--- TonWalletContest/ContentView.swift
+++ TonWalletContest/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let tonManager = TonWalletManager()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button("Create Key") {
+                Task {
+                    do {
+                        let key = try await tonManager.createKey()
+                        await print(try key.words(password: tonManager.passcode.data(using: .utf8)!))
+                        
+                        let wallet = try await tonManager.createWallet3(key: key, revision: .r2)
+                        let contract = wallet.contract
+                        print("Contract info: ", contract.info)
+                        try await print("Walley public key", wallet.publicKey)
+                    } catch {
+                        print("Got error:", error.localizedDescription)
+                    }
+                    
+                }
+            }
         }
         .padding()
     }
