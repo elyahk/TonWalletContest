99ef747755c36fdf05688645b74f2eaa65a62df4
elyahk <eldorbek.nusratov.98@gmail.com>
2023-04-08 15:01:15 +0200

    Show passcode, and open new confirm passcode view

--- TonWalletContest/View/Authorazation/Passcode/PasscodeReducer.swift
+++ TonWalletContest/View/Authorazation/Passcode/PasscodeReducer.swift
@@ -7,8 +7,50 @@ struct PasscodeReducer: ReducerProtocol {
         var id: UUID = .init()
         var key: Key
         var words: [String]
-        var password: String = ""
-        var keyboard: Bool = true
+        var passcode: String = ""
+        var showKeyboad: Bool = true
+        var confirmPasscode: ConfirmPasscodeReducer.State?
+    }
+
+    enum Action: Equatable {
+        case passwordAdded(password: String)
+        case confirmPasscode(ConfirmPasscodeReducer.Action)
+        case showConfirm(oldPasscode: String)
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case let .passwordAdded(passcode):
+            state.passcode = passcode
+            if passcode.count == 4 {
+                state.showKeyboad = false
+                
+                return .run { send in
+                    try await Task.sleep(nanoseconds: 200_000_000)
+                    await send(.showConfirm(oldPasscode: passcode))
+                }
+            }
+            
+            return .none
+            
+        case let .showConfirm(oldPasscode):
+            state.confirmPasscode = .init(key: state.key, words: state.words, oldPasscode: oldPasscode)
+            return .none
+            
+        case .confirmPasscode:
+            return .none
+        }
+    }
+}
+
+struct ConfirmPasscodeReducer: ReducerProtocol {
+    struct State: Equatable, Identifiable {
+        var id: UUID = .init()
+        var key: Key
+        var words: [String]
+        var oldPasscode: String
+        var passcode: String = ""
+        var showKeyboad: Bool = true
     }
 
     enum Action: Equatable {
@@ -17,8 +59,13 @@ struct PasscodeReducer: ReducerProtocol {
     
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
-        case let .passwordAdded(password):
+        case let .passwordAdded(passcode):
+            print(passcode)
+            if passcode.count == 4 {
+                state.passcode = ""
+            }
             return .none
         }
     }
 }
+
--- TonWalletContest/View/Authorazation/Passcode/PasscodeView.swift
+++ TonWalletContest/View/Authorazation/Passcode/PasscodeView.swift
@@ -3,7 +3,7 @@ import ComposableArchitecture
 
 struct PasscodeView: View {
     let store: StoreOf<PasscodeReducer>
-    @State var isKeyboardShown = true
+    @State var show: Bool = false
     
     init(store: StoreOf<PasscodeReducer>) {
         self.store = store
@@ -12,21 +12,104 @@ struct PasscodeView: View {
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             VStack {
-                Spacer()
-                ZStack {
-                    CustomTextField(text: Binding(
-                        get: { viewStore.password },
-                        set: { value, _ in
-                            viewStore.send(.passwordAdded(password: value))
-                        }), isFirstResponder: true
-                    )
-                    .frame(maxWidth: .infinity, maxHeight: 60)
-
-                    Button("Title") {
-                        
+                NavigationLink(
+                    isActive: Binding(get: {
+                        viewStore.state.confirmPasscode != nil
+                    }, set: { _ in }),
+                    destination: {
+                        IfLetStore(self.store.scope(
+                            state: \.confirmPasscode, action: PasscodeReducer.Action.confirmPasscode)) { store in
+                                ConfirmPasscodeView(store: store)
+                            }
+                    },
+                    label: {
+                        Color.clear
                     }
-                    .frame(maxWidth: .infinity, maxHeight: 60)
-                    .background(Color.green)
+                )
+
+                ZStack {
+                    LegacyTextField(
+                        text: Binding(
+                            get: { viewStore.passcode },
+                            set: { value, _ in
+                                viewStore.send(.passwordAdded(password: value))
+                            }),
+                        isFirstResponder: Binding(
+                            get: { viewStore.showKeyboad },
+                            set: { value, _ in
+                                    
+                            })
+                    )
+                    .frame(width: 10, height: 0)
+//                    .hidden()
+
+//                    Button("Title") {
+//                        sdf
+//                    }
+//                    .frame(maxWidth: .infinity, maxHeight: 60)
+//                    .background(Color.green)
+                }
+                
+                Button("Options") {
+                    show = true
+                }
+            }
+            .sheet(isPresented: $show) {
+                Text("Sheet")
+            }
+        }
+    }
+}
+
+struct ConfirmPasscodeView: View {
+    let store: StoreOf<ConfirmPasscodeReducer>
+    
+    init(store: StoreOf<ConfirmPasscodeReducer>) {
+        self.store = store
+    }
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+//                NavigationLink(
+//                    isActive: Binding(get: {
+//                        viewStore.state.confirmPasscode != nil
+//                    }, set: { _ in }),
+//                    destination: {
+//                        PasscodeView(store: .init(
+//                            initialState: viewStore.state, reducer: PasscodeReducer()))
+//                    },
+//                    label: {
+//                        Color.clear
+//                    }
+//                )
+
+                ZStack {
+                    TextField("", text: Binding(
+                        get: { viewStore.passcode },
+                        set: { viewStore.send(.passwordAdded(password: $0)) }
+                    ))
+                    .hidden()
+                    LegacyTextField(
+                        text: Binding(
+                            get: { viewStore.passcode },
+                            set: { value, _ in
+                                viewStore.send(.passwordAdded(password: value))
+                            }),
+                        isFirstResponder: Binding(
+                            get: { viewStore.showKeyboad },
+                            set: { value, _ in
+                                    
+                            })
+                    )
+                    .frame(width: 10, height: 0)
+//                    .hidden()
+
+//                    Button("Title") {
+//                        sdf
+//                    }
+//                    .frame(maxWidth: .infinity, maxHeight: 60)
+//                    .background(Color.green)
                 }
                 
                 Button("Options") { }
@@ -34,6 +117,7 @@ struct PasscodeView: View {
         }
     }
 }
+
 
 struct PasscodeView_Previews: PreviewProvider {
     static var previews: some View {
