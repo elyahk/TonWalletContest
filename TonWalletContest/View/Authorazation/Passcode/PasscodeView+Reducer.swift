7ef287d81f7f9fa77598c2ef6f76a140f7afbd14
elyahk <eldorbek.nusratov.98@gmail.com>
2023-04-15 09:28:18 +0200

    Move reducer to own file

--- TonWalletContest/View/Authorazation/Passcode/PasscodeView+Reducer.swift
+++ TonWalletContest/View/Authorazation/Passcode/PasscodeView+Reducer.swift
@@ -1,6 +1,57 @@
 import SwiftUI
 import ComposableArchitecture
 
+struct PasscodeReducer: ReducerProtocol {
+    struct State: Equatable, Identifiable {
+        var id: UUID = .init()
+        var passcode: String = ""
+        var showKeyboad: Bool = true
+        @PresentationState var confirmPasscode: ConfirmPasscodeReducer.State?
+    }
+
+    enum Action: Equatable {
+        case passwordAdded(password: String)
+        case confirmPasscode(PresentationAction<ConfirmPasscodeReducer.Action>)
+        case showConfirm(oldPasscode: String)
+    }
+
+    var body: some ReducerProtocolOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .passwordAdded(passcode):
+                state.passcode = passcode
+                if passcode.count == 4 {
+                    state.showKeyboad = false
+
+                    return .run { send in
+                        try await Task.sleep(nanoseconds: 200_000_000)
+                        await send(.showConfirm(oldPasscode: passcode))
+                    }
+                }
+
+                return .none
+
+            case let .showConfirm(oldPasscode):
+                state.confirmPasscode = .init(oldPasscode: oldPasscode)
+                return .none
+
+            case .confirmPasscode(.dismiss):
+                state.passcode = ""
+                state.showKeyboad = true
+
+                return .none
+
+            case .confirmPasscode:
+                return .none
+            }
+        }
+        .ifLet(\.$confirmPasscode, action: /Action.confirmPasscode) {
+            ConfirmPasscodeReducer()
+        }
+    }
+}
+
+
 struct PasscodeView: View {
     let store: StoreOf<PasscodeReducer>
     @State var show: Bool = false
@@ -53,53 +104,6 @@ struct PasscodeView: View {
             }
             .sheet(isPresented: $show) {
                 Text("Sheet")
-            }
-        }
-    }
-}
-
-struct ConfirmPasscodeView: View {
-    let store: StoreOf<ConfirmPasscodeReducer>
-    
-    init(store: StoreOf<ConfirmPasscodeReducer>) {
-        self.store = store
-    }
-
-    struct ViewState: Equatable {
-        var passcode: String = ""
-        var showKeyboad: Bool = true
-
-        init(state: ConfirmPasscodeReducer.State) {
-            self.passcode = state.passcode
-            self.showKeyboad = state.showKeyboad
-        }
-    }
-
-    var body: some View {
-        WithViewStore(self.store, observe: ViewState.init) { viewStore in
-            VStack {
-                Text("Confirm")
-                ZStack {
-                    TextField("", text: Binding(
-                        get: { viewStore.passcode },
-                        set: { viewStore.send(.passwordAdded(password: $0)) }
-                    ))
-                    .hidden()
-
-                    LegacyTextField(
-                        text: Binding(
-                            get: { viewStore.passcode },
-                            set: { value, _ in
-                                viewStore.send(.passwordAdded(password: value))
-                            }),
-                        isFirstResponder: Binding(
-                            get: { viewStore.showKeyboad },
-                            set: { value, _ in
-                                    
-                            })
-                    )
-                    .frame(width: 10, height: 0)
-                }
             }
         }
     }
