535e416a1a440c2ecff3ef5c89452cb5a99b5d78
elyahk <eldorbek.nusratov.98@gmail.com>
2023-04-16 11:44:38 +0200

    Make Wallet view page

--- TonWalletContest/View/Authorazation/ImportSuccess/ImportSuccessReducer.swift
+++ TonWalletContest/View/Authorazation/ImportSuccess/ImportSuccessReducer.swift
@@ -5,20 +5,12 @@ import Foundation
 struct ImportSuccessReducer: ReducerProtocol {
     struct State: Equatable, Identifiable {
         var id: UUID = .init()
-        var testWords: IdentifiedArrayOf<Word> = IdentifiedArrayOf(uniqueElements: (1...24).map { Word(key: $0) })
         @PresentationState var destination: Destination.State?
     }
 
     enum Action: Equatable {
         case destination(PresentationAction<Destination.Action>)
-        case continueButtonTapped
-        case wordChanged(id: Word.ID, value: String)
-        case autoFillCorrectWords
-
-        enum Alert: Equatable {
-            case seeWords
-            case dismiss
-        }
+        case viewWalletButtonTapped
     }
 
     @Dependency(\.dismiss) var presentationMode
@@ -26,33 +18,8 @@ struct ImportSuccessReducer: ReducerProtocol {
     var body: some ReducerProtocolOf<Self> {
         Reduce { state, action in
             switch action {
-            case .continueButtonTapped:
-                //                if state.isCorrectRecieveddWords() {
-                //                    state.destination = .passcode(.init())
-                //                } else {
-                //                    state.destination = .alert(.init(
-                //                        title: TextState("Incorrect words"),
-                //                        message: TextState("The secret words you have entered do not match the ones in the list."),
-                //                        primaryButton: .default(TextState("See words"), action: .send(.seeWords)),
-                //                        secondaryButton: .default(TextState("Try again"), action: .send(.dismiss))
-                //                    ))
-                //                }
+            case .viewWalletButtonTapped:
 
-                return .none
-
-            case let .wordChanged(id, value):
-                state.testWords[id: id]?.recivedWord = value
-                return .none
-
-            case .autoFillCorrectWords:
-
-                return .none
-
-            case .destination(.presented(.alert(.seeWords))):
-                return .fireAndForget { await self.presentationMode() }
-
-            case .destination(.presented(.alert(.dismiss))):
-                state.destination = nil
                 return .none
 
             case .destination:
@@ -66,35 +33,23 @@ struct ImportSuccessReducer: ReducerProtocol {
 }
 
 extension ImportSuccessReducer {
-    struct Word: Identifiable, Equatable {
-        var id: UUID = .init()
-        var key: Int
-        var recivedWord: String = ""
-    }
-}
-
-extension ImportSuccessReducer {
     struct Destination: ReducerProtocol {
         enum State: Equatable, Identifiable {
-            case passcode(PasscodeReducer.State)
-            case alert(AlertState<ImportSuccessReducer.Action.Alert>)
+            case wallet(PasscodeReducer.State)
 
             var id: AnyHashable {
                 switch self {
-                case let .passcode(state):
-                    return state.id
-                case let .alert(state):
+                case let .wallet(state):
                     return state.id
                 }
             }
         }
         enum Action: Equatable {
-            case passcode(PasscodeReducer.Action)
-            case alert(ImportSuccessReducer.Action.Alert)
+            case wallet(PasscodeReducer.Action)
         }
 
         var body: some ReducerProtocolOf<Self> {
-            Scope(state: /State.passcode, action: /Action.passcode) {
+            Scope(state: /State.wallet, action: /Action.wallet) {
                 PasscodeReducer()
             }
         }
--- TonWalletContest/View/Authorazation/ImportSuccess/ImportSuccessView.swift
+++ TonWalletContest/View/Authorazation/ImportSuccess/ImportSuccessView.swift
@@ -2,80 +2,40 @@ import SwiftUI
 import ComposableArchitecture
 
 struct ImportSuccessView: View {
-    @Environment(\.presentationMode) var presentationMode
+    let store: StoreOf<ImportSuccessReducer>
 
-    @State var showingAlert: Bool = false
-    let store: StoreOf<ImportPhraseReducer>
-
-    init(store: StoreOf<ImportPhraseReducer>) {
+    init(store: StoreOf<ImportSuccessReducer>) {
         self.store = store
     }
 
-    struct ViewState: Equatable {
-        var testWords: IdentifiedArrayOf<ImportPhraseReducer.Word>
-
-        init(state: ImportPhraseReducer.State) {
-            self.testWords = state.testWords
-        }
-    }
-
     var body: some View {
-        WithViewStore(self.store, observe: ViewState.init) { viewStore in
-            ScrollView {
-                LottieView(name: "list", loop: .loop)
-                    .frame(width: 124, height: 124, alignment: .center)
-                Text("24 Secret Words")
+        VStack {
+            Spacer()
+            LottieView(name: "boomstick", loop: .playOnce)
+                .frame(width: 124, height: 124, alignment: .center)
+            Text("Your wallet has just been imported!")
+                .fontWeight(.semibold)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 5)
+            Spacer()
+            // Create My Wallet app
+            NavigationLinkStore (
+                self.store.scope(state: \.$destination, action: ImportSuccessReducer.Action.destination)
+            ) {
+
+            } destination: { store in
+                Text("New page")
+            } label: {
+                Text("View my wallet")
                     .fontWeight(.semibold)
-                    .font(.title)
-                    .padding()
-                Text("You can restore access to your wallet by entering 24 words you wrote when down you creating the wallet.")
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 32)
-                    .padding(.bottom, 30)
-
-                ForEach(viewStore.testWords) { word in
-                    HStack {
-                        Text("\(word.key). ")
-                            .foregroundColor(.gray)
-                            .padding(.vertical, 15)
-                            .frame(width: 40, alignment: .trailing)
-                        TextField("", text: Binding(
-                            get: { word.recivedWord },
-                            set: { value, _ in
-                                viewStore.send(.wordChanged(id: word.id, value: value))
-                            }
-                        ))
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color("LightGray"))
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                    .background(Color.accentColor)
                     .cornerRadius(12)
-                    .padding(.horizontal, 48)
-                    .padding(.bottom, 10)
-                }
-
-                NavigationLinkStore(
-                    self.store.scope(state: \.$destination, action: ImportPhraseReducer.Action.destination),
-                    state: /ImportPhraseReducer.Destination.State.passcode,
-                    action: ImportPhraseReducer.Destination.Action.passcode
-                ) {
-                    viewStore.send(.continueButtonTapped)
-                } destination: { store in
-                    PasscodeView(store: store)
-                } label: {
-                    Text("Continue")
-                        .frame(maxWidth: .infinity, minHeight: 50, alignment: .center)
-                        .customBlueButtonStyle()
-                }
+                    .padding([.leading, .trailing], 48)
+                    .padding(.bottom, 124)
             }
-            .alert(
-                self.store.scope(
-                    state: { guard case let .alert(state) = $0.destination else { return nil }
-                        return state
-                    },
-                    action: { ImportPhraseReducer.Action.destination(.presented(.alert($0)))}
-                ),
-                dismiss: .dismiss
-            )
         }
     }
 }
@@ -85,7 +45,7 @@ struct ImportSuccessView_Previews: PreviewProvider {
         NavigationView {
             ImportSuccessView(store: .init(
                 initialState: .init(),
-                reducer: ImportPhraseReducer()
+                reducer: ImportSuccessReducer()
             ))
         }
     }
