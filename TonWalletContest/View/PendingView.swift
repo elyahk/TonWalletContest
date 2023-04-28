b269639d633594b61bd8e3ba92bc183a2fbb5b36
slavny11 <slavny45@gmail.com>
2023-04-28 12:09:37 +0200

    Added sticker with money animation

---
+++ TonWalletContest/View/PendingView.swift
@@ -0,0 +1,45 @@
+//
+//  PendingView.swift
+//  TonWalletContest
+//
+//  Created by Viacheslav on 28/04/23.
+//
+
+import SwiftUI
+
+struct PendingView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            LottieView(name: "money", loop: .playOnce)
+                .frame(width: 124, height: 124, alignment: .center)
+            Text("Sending TON")
+                .fontWeight(.semibold)
+                .font(.title)
+                .padding(.bottom, 5)
+            Text("Please wait a few seconds for your transaction to be processed…")
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 32)
+            Spacer()
+
+//            NavigationLinkStore(
+//                self.store.scope(state: \.$recoveryPhrase, action: CongratulationReducer.Action.recoveryPhrase)
+//            ) {
+//                ViewStore(store).send(.proceedButtonTapped)
+//            } destination: { store in
+//                RecoveryPhraseView(store: store)
+//            } label: {
+//                Text("Proceed")
+//                    .frame(maxWidth: .infinity, minHeight: 50, alignment: .center)
+//                    .customBlueButtonStyle()
+//                    .padding(.bottom, 90)
+//            }
+        }
+    }
+}
+
+struct PendingView_Previews: PreviewProvider {
+    static var previews: some View {
+        PendingView()
+    }
+}
